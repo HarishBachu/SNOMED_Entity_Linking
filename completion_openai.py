06bc343bbe191f14f66208b948ef5b22f3a2ee69
HarishBachu <bachuharish577@gmail.com>
2024-01-11 05:58:46 +0000

    Initial Commit

--- completion_openai.py
+++ completion_openai.py
@@ -1,5 +1,5 @@
 import openai
-
+from openai import OpenAI
 
 MODEL = "gpt-3.5-turbo"
 TEMPERATURE = 0
@@ -7,10 +7,30 @@ TEMPERATURE = 0
 # 'more likely to be correct and efficient'. Instead, 0 is used for consistency.
 
 with open('openai.key', 'r') as file:
-    openai.api_key = file.read().rstrip()
+    api_key = file.read().rstrip()
 
 def create_chat_completion(prompts, model=MODEL, temperature=TEMPERATURE, **kwargs):
-    response = openai.ChatCompletion.create(messages=prompts, model=model, temperature=temperature, **kwargs)
+    client = OpenAI(api_key = api_key)
+    response = client.chat.completions.create(messages=[
+      {
+        "role" : "user", 
+        "content" : "Hi There"
+      }
+    ], model=model, temperature=temperature, **kwargs)
     return response["choices"][0]["message"]["content"]
 
+# client = OpenAI(
+#     # This is the default and can be omitted
+#     api_key=os.environ.get("OPENAI_API_KEY"),
+# )
+
+# chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "Say this is a test",
+#         }
+#     ],
+#     model="gpt-3.5-turbo",
+# )
 
