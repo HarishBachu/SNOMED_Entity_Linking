5c0cb4b87742fa29676bb848611b910720c3ff38
HarishBachu <bachuharish577@gmail.com>
2024-01-17 12:42:02 +0000

    Model accuracy estimate

--- evaluation.py
+++ evaluation.py
@@ -2,25 +2,40 @@ import numpy as np
 import pandas as pd 
 import argparse
 
-def micro_iou(df_true, df_pred, note_id):
+def slice_dfs(df_true, df_pred, note_id):
   # Slice ground truth and predictions for specific class (note_id) 
   df_true = df_true[df_true["note_id"]==note_id]
   df_pred = df_pred[df_pred["note_id"]==note_id]
 
   # Find distinct list of extracted entities 
   y_true = df_true["concept_id"].tolist() 
-  y_pred = df_pred["concept_id"].tolist() 
+  y_pred = df_pred["concept_id"].tolist()
+  return y_true, y_pred
 
+def micro_iou(df_true, df_pred, note_id):
+  y_true, y_pred = slice_dfs(df_true, df_pred, note_id)
   return np.intersect1d(y_true, y_pred).shape[0] / np.union1d(y_true, y_pred).shape[0] 
+
+def micro_acc(df_true, df_pred, note_id):
+  y_true, y_pred = slice_dfs(df_true, df_pred, note_id)
+  n_correct_preds = np.intersect1d(y_true, y_pred).shape[0]
+  return n_correct_preds / (len(y_true) - n_correct_preds)
 
 def macro_iou(df_true, df_pred):
   #Initialize empty array for micro IOU for each class 
   iou = []
-
+  acc = [] 
+  
   #Calculate Macro IOU as mean of micro IOUs
   for note_id in df_true["note_id"].unique():
     iou.append(micro_iou(df_true, df_pred, note_id))
-  return np.mean(iou)
+    acc.append(micro_acc(df_true, df_pred, note_id))
+
+  metric_dict = {
+    "Macro IOU" : np.mean(iou), 
+    "Accuracy" : np.mean(acc)
+  }
+  return metric_dict 
 
 DEFAULT_FILE = "test.csv"
 
